c3538aa7e7ec382a0a0996b5149f950e32b44d33
Cristian Cordoba <cristian56cordoba@gmail.com>
2024-03-26 13:22:16 -0300

    Elevando al cuadrado

---
+++ ejercicio4.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void cuadradoNumero(int *n);
+
+int main(){
+    int *n;
+    int num;
+    printf("Ingresar el numero: ");
+    scanf("%d", &num);
+
+    n = &num;
+
+    cuadradoNumero(n);
+
+    printf("\nEl valor elevado al cuadrado es: %d", *n);
+    
+}
+
+void cuadradoNumero(int *n){
+    *n = (*n)*(*n);
+}
+
+
